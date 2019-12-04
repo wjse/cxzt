53415c42b91226bede9cccb2d6c5ba9c21b853fc
jia.wu <jia.wu@q7link.com>
2019-12-04 23:04:04 +0800

    first commit

---
+++ src/test/java/com/k66/cxzt/CxztApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k66.cxzt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxztApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
